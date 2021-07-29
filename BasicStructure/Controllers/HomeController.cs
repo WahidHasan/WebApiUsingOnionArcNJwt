dfe28c2b48a0604693d33929e0b1aaaa98da2f9c
wahid <hasanalwahid@gmail.com>
2021-07-29 13:28:42 +0600

    Add project files.

---
+++ BasicStructure/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OA_Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicStructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        private readonly IProductRepo _repo;
+
+        public HomeController(IProductRepo repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _repo.GetAllProducts();
+            return View(products);
+        }
+    }
+}
---
+++ OA_Service/ProductRepo.cs
@@ -0,0 +1,30 @@
+﻿using OA_DataAccess;
+using OA_Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Service
+{
+    public class ProductRepo : IProductRepo
+    {
+        private IRepository<Product> _repository;
+
+        public ProductRepo(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _repository.GetAll();
+        }
+
+        public Product GetProductById(int Id)
+        {
+            return _repository.GetById(Id);
+        }
+    }
+}
